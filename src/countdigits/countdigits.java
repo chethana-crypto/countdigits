385165084def736f18d65ac393a641c6a7bc8a73
chethana-crypto <69971162+chethana-crypto@users.noreply.github.com>
2021-09-13 21:17:38 +0530

    first commit

---
+++ src/countdigits/countdigits.java
@@ -0,0 +1,11 @@
+package countdigits;
+
+public class countdigits {
+	public static void main(String args[]) {
+		int[] digits= {100,92,65,87,93,55,64,48,99,88,82,81,73,66};
+		int count=digits.length;
+		System.out.println("the count of N digits is "+count);
+
+
+	}
+}
